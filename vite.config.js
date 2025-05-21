6043c085c8b7f7387f649c1f5e201a0a156dd3e1
natchaphatso <natchaphatso@gmail.com>
2025-05-21 17:30:11 +0700

    change repo

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/cc20-yup-validate-form",
+  base: "/cc20-yup-test-app",
 });
